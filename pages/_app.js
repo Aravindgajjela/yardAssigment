c728cac201e61e140d355d5ef30ba81f604011b3
unknown <anormousaravind@gmail.com>
2025-02-05 20:44:39 +0530

    n

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/EditTaskModal.css';
+import '../styles/TaskForm.css';
+import '../styles/TaskItem.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
